3669701af693fedf884128931609d685bbac7c32
12007661 - Killer Lorenz <killer@student.tugraz.at>
2021-04-22 09:44:21 +0200

    improve calculateRotation

--- TreasureTile.cpp
+++ TreasureTile.cpp
@@ -9,10 +9,12 @@
 
 #include "TreasureTile.hpp"
 
+#include <algorithm>
+
 TreasureTile::TreasureTile(Treasure* treasure) : treasure_{treasure}, collected_{false}
 {
   setType(TileType::T);
-  setRotation(calculateRotation(treasure.getTreasureId()));
+  setRotation(calculateRotation(treasure->getTreasureId()));
 }
 
 Treasure* TreasureTile::getTreasure()
@@ -42,7 +44,7 @@ void TreasureTile::collectTreasure()
 
 std::string TreasureTile::getTreasureId()
 {
-  int treasure_id = treasure_.getTreasureId();
+  int treasure_id = treasure_->getTreasureId();
   std::string treasure = "T";
 
   if (treasure_id < 10)
@@ -81,15 +83,16 @@ Rotation TreasureTile::calculateRotation(size_t treasure_id)
   std::vector<size_t> deg_90 = {3, 4, 7};
   std::vector<size_t> deg_180 = {8, 11, 12};
   std::vector<size_t> deg_270 = {6, 9, 10};
-  if (std::find(deg_0.begin(), deg_0.end(), treasure_id))
+
+  if (std::count(deg_0.begin(), deg_0.end(), treasure_id))
   {
     return Rotation::DEG0;
   }
-  else if (std::find(deg_90.begin(), deg_90.end(), treasure_id))
+  else if (std::count(deg_90.begin(), deg_90.end(), treasure_id))
   {
     return Rotation::DEG90;
   }
-  else if (std::find(deg_180.begin(), deg_180.end(), treasure_id))
+  else if (std::count(deg_180.begin(), deg_180.end(), treasure_id))
   {
     return Rotation::DEG180;
   }
